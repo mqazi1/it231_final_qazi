8eed3e9f2ff30a25f9047ea7632aa9a2d05ad27e
Maria Qazi <37481373+mqazi1@users.noreply.github.com>
2026-05-28 15:18:12 -0500

    Created all the files

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require("express"), 
+    app = express(),
+    errorController = require("./controllers/errorController"),
+    homeController = require("./controllers/homeController"),
+
+    mongoose = require("mongoose"),
+    layouts = require("express-ejs-layouts");
