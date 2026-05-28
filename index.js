001db006a74c7deb6c5e890f5ec553d2dc715111
Maria Qazi <37481373+mqazi1@users.noreply.github.com>
2026-05-28 15:42:48 -0500

    Set up the index.js file

--- index.js
+++ index.js
@@ -5,3 +5,29 @@ const express = require("express"),
 
     mongoose = require("mongoose"),
     layouts = require("express-ejs-layouts");
+
+mongoose.connect(
+    "mongodb+srv://mqazi1:Mendes98@cluster0.gevvjfr.mongodb.net/habits?appName=Cluster0"
+)
+
+const db = mongoose.connection;
+db.once("open", () => {
+    console.log("Connection to MongoDB has been successfully created!")
+});
+
+app.set("port", process.env.PORT || 3000);
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(layouts);
+app.use(
+    express.urlencoded({
+        extended: false,
+    })
+);
+
+app.use(express.json());
+
+
+app.listen(app.get("port"), () => {
+    console.log(`Success! Connection is secured and the server is live at http://localhost:${app.get("port")}`);
+});
